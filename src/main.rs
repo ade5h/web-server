d6a91709c6c70f53d97aa647003c36e932b3caa1
Adesh Attavar <adesh.attavar@gmail.com>
2024-06-02 13:08:38 +0530

    Throwing 404 when the path is anything other than /

--- src/main.rs
+++ src/main.rs
@@ -17,14 +17,16 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
 
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let request_line: String = buf_reader.lines().next().unwrap().unwrap();
 
-    let status_line = "HTTP/1.1 200 OK";
-    let html_contents = fs::read_to_string("hello.html").unwrap();
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let html_contents = fs::read_to_string(filename).unwrap();
+
     let length = html_contents.len();
 
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{html_contents}");
